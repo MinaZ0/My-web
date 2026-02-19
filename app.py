79dad5a904e27f04e1a4c9a01c3bbc1e8f3d9ace
MinaZ0 <LmyourGuyZzz@gmail.com>
2026-02-19 14:01:23 +0000

    feat: add mockup product data to index route

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app.config['SECRET_KEY'] = 'dev-key-123'
 
 @app.route('/')
 def index():
-    return "<h1>E-Commerce Project is Running!</h1>"
+    products = [
+        {'id': 1, 'name': 'MacBook Pro', 'price': 45900, 'img': 'https://via.placeholder.com/150'},
+        {'id': 2, 'name': 'iPhone 15', 'price': 32900, 'img': 'https://via.placeholder.com/150'},
+        {'id': 3, 'name': 'iPad Air', 'price': 23900, 'img': 'https://via.placeholder.com/150'}
+    ]
+    return render_template('index.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
